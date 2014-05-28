7002a6583915ad000faf0edb7f2943e768199d43
Benjamin Kovach <bkovach13@gmail.com>
2014-05-28 18:09:58 -0400

    Add error detection in Identity.cobind

--- Identity/MonadIdentity.js
+++ Identity/MonadIdentity.js
@@ -2,5 +2,10 @@ var Identity = require('./Identity');
 
 module.exports = {
   pure : function(x){ return new Identity(x); },
-  copure : function(idx){ return idx.val; }
+  copure : function(idx){ 
+    if(!(idx instanceof Identity && idx.type === 'Identity')){
+      throw new Error('Expected type Identity in first argument of MonadIdentity.copure, but got ' + typeof idx);
+    }
+    return idx.val;
+  }
 }
