c608d44f088a91cbdeca31c09be97298ab20634e
Benjamin Kovach <bkovach13@gmail.com>
2014-05-31 17:59:29 -0400

    Add 'famine notation'

--- Examples/IdentityExample.js
+++ Examples/IdentityExample.js
@@ -8,3 +8,11 @@ var result = five.chain(function(n){
 });
 
 console.log(result.toString()); // Identity 14
+
+// or in famine notation
+
+var _result = famine.chain(five, function(n){
+  return Identity.of(n + 9);
+});
+
+console.log(_result.toString()); // Identity 14
--- famine.js
+++ famine.js
@@ -5,7 +5,56 @@ var Either = require('./Either/Either'),
     Maybe = require('./Maybe/Maybe'),
     State = require('./State/State'),
     Writer = require('./Writer/Writer'),
-    Tuple = require('./Tuple/Tuple');
+    Tuple = require('./Tuple/Tuple'),
+    TC = require('./TypeChecker');
+
+var empty = function(a){
+  if(TC.isSemigroup(a)){
+    return a.value.empty ? a.value.empty() : a.value.constructor.empty();
+  };
+};
+
+var concat = function(a, b){
+  if(TC.isMonoid(a) && TC.isMonoid(b)){
+    return a.concat(b);
+  };
+};
+
+var map = function(a, f){
+  if(TC.isFunctor(a) && TC.isFunction(f)){
+    return a.map(f);
+  };
+};
+
+var ap = function(a, b){
+  if(TC.isApplicative(a) && TC.isApplicative(b)){
+    return a.ap(b);
+  };
+};
+
+var chain = function(a, f){
+  if(TC.isChain(a) && TC.isFunction(f)){
+    return a.chain(f);
+  };
+};
+
+var extend = function(a, f){
+  if(TC.isExtend(a) && TC.isFunction(f)){
+    return a.extend(f);
+  };
+};
+
+var compose = function(f, g){
+  return function(x){
+    return f(g(x));
+  };
+};
+
+var identity = function(n){ return n; };
+
+var constant = function(y){ 
+  return function(){ return y; }; 
+};
 
 module.exports = {
   Either : Either,
@@ -16,6 +65,13 @@ module.exports = {
   Writer : Writer,
   Tuple : Tuple,
   Monoid : Monoid,
-  id : function(n){ return n; },
-  const : function(y){ return function(){ return y; } }
+  empty : empty,
+  concat : concat,
+  map : map,
+  ap : ap,
+  chain : chain,
+  extend : extend,
+  compose : compose,
+  identity : identity,
+  constant : constant
 }
