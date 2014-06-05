a6bc2cbb2361cf30fcc92b765f4facb462a57fa7
Benjamin Kovach <bkovach13@gmail.com>
2014-06-05 14:40:51 -0400

    Move List to Fantasy Land spec

--- Examples/ListExample.js
+++ Examples/ListExample.js
@@ -3,12 +3,12 @@ var famine    = require('../famine'),
 
 var xs = new List([1, 2, 3]);
 
-var withSquares = xs.bind(function(n){
+var withSquares = xs.chain(function(n){
   return new List([n, n*n]);
 });
 
-console.log(withSquares.toString()); // List [1, 1, 2, 4, 3, 9]
+console.log(withSquares.toString()); // [1, 1, 2, 4, 3, 9]
 
-var withSquaresApplicative = new List([famine.id, function(n){ return n*n; }]).apply(xs);
+var withSquaresApplicative = new List([famine.identity, function(n){ return n*n; }]).ap(xs);
 
-console.log(withSquaresApplicative.toString()); // List [1, 2, 3, 1, 4, 9]
+console.log(withSquaresApplicative.toString()); // [1, 2, 3, 1, 4, 9]
--- List/List.js
+++ List/List.js
@@ -8,66 +8,61 @@ var List = function(vals){
   }
 
   this.val = vals;
+  this.type = 'List';
 
   if(!(this.val instanceof Array)){
     throw new Error('Expected Array but got something else in the constructor of List');
   }
-  
-  // fmap :: (a -> b) -> List a -> List b
-  this.fmap = function(f){
-    if(typeof f !== 'function'){
-      throw new Error('Expected function but got ' + typeof f + ' in the first argument of List.fmap');
-    }
-    var ret = [];
-    for(var i = 0; i < this.val.length; i++){
-      ret.push(f(this.val[i]));
-    }
-    return new List(ret);
-  }
-
-  // apply :: List (a -> b) -> List a -> List b
-  this.apply = function(list){
-    // vals is a list of functions
-    var ret = [];
-    for(var i = 0; i < this.val.length; i++){
-      var cur = (this.val)[i];
-      if(typeof cur !== 'function'){
-        throw new Error('Expected function but got ' + typeof cur + ' in the ' + i + 'th index of vals in List.apply');
-      }
-      var curList = list.fmap(cur);
-      ret = ret.concat(curList.val);
-    }
-    return new List(ret);
-  }
-  // bind :: List a -> (a -> List b) -> List b
-  this.bind = function(f){
-    if(typeof f !== 'function'){
-      throw new Error('Expected function but got ' + typeof f + ' in the first argument of List.bind');
-    }
-    var lists = this.fmap(f).val,
-        ret = [];
-    for(var i = 0; i < lists.length; i++){
-      var curList = lists[i];
-      if(!(curList.type === 'List' && curList instanceof List) ){
-        throw new Error('Expected List but got ' + typeof curList + ' in the ' + i + 'th index of vals in List.bind');
-      }
-      ret = ret.concat(curList.val);
-    }
-    return new List(ret);
-  }
-
-  this.functor = true;
-  this.applicative = true;
-  this.monad = true;
-  this.type = 'List';
 }
 
-List.pure = function(x){
+List.prototype.map = function(f){
+  if(typeof f !== 'function'){
+    throw new Error('Expected function but got ' + typeof f + ' in the first argument of List.map');
+  }
+  var ret = [];
+  for(var i = 0; i < this.val.length; i++){
+    ret.push(f(this.val[i]));
+  }
+  return new List(ret);
+}
+
+List.prototype.ap = function(list){
+  // vals is a list of functions
+  var ret = [];
+  for(var i = 0; i < this.val.length; i++){
+    var cur = (this.val)[i];
+    if(typeof cur !== 'function'){
+      throw new Error('Expected function but got ' + typeof cur + ' in the ' + i + 'th index of vals in List.ap');
+    }
+    var curList = list.map(cur);
+    ret = ret.concat(curList.val);
+  }
+  return new List(ret);
+}
+
+List.prototype.chain = function(f){
+  if(typeof f !== 'function'){
+    throw new Error('Expected function but got ' + typeof f + ' in the first argument of List.chain');
+  }
+  var lists = this.map(f).val,
+      ret = [];
+  for(var i = 0; i < lists.length; i++){
+    var curList = lists[i];
+    if(!(curList.type === 'List' && curList instanceof List) ){
+      throw new Error('Expected List but got ' + typeof curList + ' in the ' + i + 'th index of vals in List.chain');
+    }
+    ret = ret.concat(curList.val);
+  }
+  return new List(ret);
+}
+
+
+List.of = function(x){
   return List([x]);
 }
 
 List.prototype.toString = function(){
-  return "List " + (this.val.toString());
+  return '[' + (this.val.toString()) + ']';
 }
 
-module.exports = List
+module.exports = List;
