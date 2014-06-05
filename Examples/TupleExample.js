655e9ab66f3cb84b71451f88641dfd60e8c95252
Benjamin Kovach <bkovach13@gmail.com>
2014-06-05 14:48:34 -0400

    Realize Tuple is just Reader

--- Examples/TupleExample.js
+++
@@ -1,9 +0,0 @@
-var famine = require('../famine'),
-    Tuple  = famine.Tuple;
-
-var doubleSecond = new Tuple(1, 'x').bind(function(second){
-  // Note we have to explicitly set `1` back here: The only info the function has is its second argument.
-  return new Tuple(1, second + second);
-});
-
-console.log(doubleSecond.toString()); // (1, 'xx')
--- Tuple/Tuple.js
+++ Tuple/Tuple.js
@@ -9,51 +9,7 @@ var Tuple = function(a, b){
 
   this.fst = a;
   this.snd = b;
-
-  // fmap :: (a -> b) -> (c, a) -> (c, b)
-  this.fmap = function(f){
-    if(typeof f !== 'function'){
-      throw new Error('Expected function but got ' + typeof f + ' in the first argument of Tuple.fmap');
-    }
-    return new Tuple(a, f(b));
-  };
-
-  // apply :: (c, (a -> b)) -> (c, a) -> (c, b)
-  this.apply = function(tuple){
-    if(!(tuple instanceof Tuple && tuple.type === 'Tuple')) {
-      throw new Error('Expected Tuple but got ' + typeof tuple + ' in the first argument of Tuple.apply');
-    }else if(!(this.snd instanceof Tuple && this.snd.type === 'Tuple')){
-      throw new Error('Expected Tuple but got ' + typeof (this.snd) + ' in the snd value of this in Tuple.apply'); 
-    }
-    return new Tuple(tuple.fst, (this.snd)(tuple.snd));
-  };
-
-  // bind :: (c, a) -> (a -> (c, b)) -> (c, b)
-  this.bind = function(f){
-    if(typeof f !== 'function'){
-      throw new Error('Expected function but got ' + typeof f + ' in the first argument of Tuple.bind');
-    }
-    
-    var res = f(b);
-    
-    if(!(res instanceof Tuple && res.type === 'Tuple')){
-      throw new Error('Expected Tuple but got ' + typeof res + ' in the return type of Tuple.apply'); 
-    }
-    
-    return res;
-  };
-
-  this.functor = true;
-  this.applicative = true;
-  this.monad = true;
-  this.type = 'Tuple';
-}
-
-Tuple.pure = function(monoid, b){
-  if(monoid.type !== 'Monoid'){
-    throw new Error('Expected Monoid but got ' + typeof monoid + ' in the first argument of Tuple.pure'); 
-  }
-  return new Tuple(monoid.mempty, b);
+  this.type = "Tuple";
 }
 
 Tuple.prototype.toString = function(){
